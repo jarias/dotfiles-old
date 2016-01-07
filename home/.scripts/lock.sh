36ea644e010d1bd99b479726d04d024a1b0b14d8
Julio Arias <jarias01@gmail.com>
2016-01-06 23:00:47 -0600

    Change default terminal and lock screen

--- home/.scripts/lock.sh
+++ home/.scripts/lock.sh
@@ -10,4 +10,4 @@ mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
 i3lock -i /tmp/screen_locked.png
 
 # Turn the screen off after a delay.
-sleep 60; pgrep i3lock && xset dpms force off
+sleep 60; pgrep slock && xset dpms force off
