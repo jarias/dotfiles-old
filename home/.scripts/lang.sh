f0f4fa616241f46bafe43c0dca59c91cc74e4aff
Julio Arias <jarias01@gmail.com>
2016-04-21 18:09:00 -0600

    Remove xkb options after lang changes

--- home/.scripts/lang.sh
+++ home/.scripts/lang.sh
@@ -1,3 +1,1 @@
 (setxkbmap -query | grep -q "layout:\s\+us") && setxkbmap es || setxkbmap us
-
-setxkbmap -device $(xinput list | grep -o -P 'AT Translated Set 2 keyboard\s+id=\d+' | sed -e 's/AT Translated Set 2 keyboard\s*id=\(.\)/\1/') -option altwin:swap_lalt_lwin
