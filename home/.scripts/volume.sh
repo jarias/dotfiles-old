44c1aef4bb55b4bfafbfe5a0ecce9c12a0d63058
Julio Arias <jarias@users.noreply.github.com>
2016-10-30 17:42:00 -0600

    Fix font sizes and experiment with gnome-i3

--- home/.scripts/volume.sh
+++ home/.scripts/volume.sh
@@ -4,15 +4,15 @@ mute=`amixer get Master | grep "Mono:" | awk '{print $6}'`
 vol=`amixer get Master | grep "Mono:" | awk '{print $4}' | tr -d '[%]'`
 if [ $mute == "[on]" ]
 then
-  if [ $vol == "100" ]
+  if [ $vol == 100 ]
   then
     echo "<span background=\"#2F343F\" foreground=\"white\">    $vol% </span>"
-  elif [ $vol \< "50" ]
+  elif [ $vol \< 50 ]
   then
     echo "<span background=\"#2F343F\" foreground=\"white\">   $vol% </span>"
   else
     echo "<span background=\"#2F343F\" foreground=\"white\">    $vol% </span>"
   fi
 else
-  echo "<span background=\"#2F343F\" foreground=\"white\">   $vol% </span>"
+  echo "<span background=\"#2F343F\" foreground=\"white\">    $vol% </span>"
 fi
