0364a7b917ef29c602e666889b4c3f2bbc2d3922
Julio Arias <jarias@users.noreply.github.com>
2017-03-31 11:19:25 -0600

    Improve temp i3blocks scripts

--- home/.scripts/temp.sh
+++ home/.scripts/temp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-TEMP=`sensors | grep ^"Physical id 0:" | awk '{print $4}' | sed 's/\+\(.*\)°C/\1/'`
+TEMP=`sensors | grep ^"Package id 0:" | awk '{print $4}' | sed 's/\+\(.*\)°C/\1/'`
 
 echo "<span background=\"#2F343F\" foreground=\"white\">   $TEMP </span>"
