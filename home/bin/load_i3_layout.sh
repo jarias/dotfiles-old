c816db3278d9256d9790fb753ae83125b0c05676
Julio Arias <jarias01@gmail.com>
2018-01-26 14:38:59 -0600

    Load default i3 layout

---
+++ home/bin/load_i3_layout.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+i3-msg "workspace 1; append_layout ~/.config/i3/workspace_1.json"
+(qutebrowser &)
+
+i3-msg "workspace 2; append_layout ~/.config/i3/workspace_2.json"
+(slack &)
+
+i3-msg "workspace 3; append_layout ~/.config/i3/workspace_3.json"
+(alacritty &)
