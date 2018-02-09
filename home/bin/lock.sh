79a91dbc8bae5a968bf6e7c95e62e8a5bc41eb34
Julio Arias <jarias01@gmail.com>
2018-02-09 17:10:34 -0600

    Update lock script

--- home/bin/lock.sh
+++ home/bin/lock.sh
@@ -2,8 +2,10 @@
 # suspend message display
 pkill -u "$USER" -USR1 dunst
 
+scrot /tmp/screenshot.png
+convert /tmp/screenshot.png -blur 0x9 /tmp/screenshotblur.png
 # lock the screen
-i3lock -n -f -e -d -c 000000 -i ~/Pictures/wallpaper.png
+i3lock -n -f -e -d -c 000000 -i /tmp/screenshotblur.png
 
 # resume message display
 pkill -u "$USER" -USR2 dunst
