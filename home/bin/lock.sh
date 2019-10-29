e0bee0e5ae1bf18535c8beda403f6fac467cc3fa
Julio Arias <jarias01@gmail.com>
2019-10-28 23:40:57 -0600

    remove bin dir

--- home/bin/lock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# suspend message display
-pkill -u "$USER" -USR1 dunst
-
-# lock the screen
-i3lock -t -n -f -e -d -c 000000 -i ~/Pictures/lock.png
-
-# resume message display
-pkill -u "$USER" -USR2 dunst
--- home/bin/mail.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/zsh
-
-BASE16_SHELL=$HOME/.config/base16-shell/
-eval "$($BASE16_SHELL/profile_helper.sh)"
-
-nvim -c 'set ft=mail' $1
